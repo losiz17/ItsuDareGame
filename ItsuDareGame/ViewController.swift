808fbab148c4590855fe642bce1c2f3391d8e28f
losiz17 <riokam.11.12@docomo.ne.jp>
2018-06-06 16:24:29 +0900

    Initial Commit

---
+++ ItsuDareGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ItsuDareGame
+//
+//  Created by 吉川莉央 on 2018/06/06.
+//  Copyright © 2018年 RioYoshikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
